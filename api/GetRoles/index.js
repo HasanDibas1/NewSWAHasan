1e8cf6f68fca7f9c5b429c9fa580e0929c346a2e
HasanDibas1 <92920719+HasanDibas1@users.noreply.github.com>
2025-12-14 00:11:53 +0300

    Create index.js

---
+++ api/GetRoles/index.js
@@ -0,0 +1,22 @@
+module.exports = async function (context, req) {
+    
+    context.log("FULL REQUEST :", req);
+
+    // Get authenticated user info
+    const clientPrincipal = req.headers["x-ms-client-principal"]
+        ? JSON.parse(Buffer.from(req.headers["x-ms-client-principal"], "base64").toString("utf8"))
+        : null;
+
+    // Default: no roles
+    let roles = [];
+
+    if (clientPrincipal) {
+        roles = clientPrincipal.userRoles || [];
+    }
+
+    context.res = {
+        body: {
+            roles: roles
+        }
+    };
+};
